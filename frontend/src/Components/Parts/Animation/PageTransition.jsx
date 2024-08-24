faaf5621391b3d9fcfc6c301b8f1608a9da0775b
weam <weamibrahim46@gmail.com>
2024-08-24 23:38:15 +0300

    update transition

--- frontend/src/Components/Parts/Animation/PageTransition.jsx
+++ frontend/src/Components/Parts/Animation/PageTransition.jsx
@@ -13,14 +13,14 @@ const PageTransition = (OriginalComponent) => {
                 initial={{ scaleX: 0 }}
                 animate={{ scaleX: 0 }}
                 exit={{ scaleX: 1 }}
-                transition={{ duration: 0.5, ease: 'easeInOut' }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
             />
             <motion.div
                 className='slide-out'
                 initial={{ scaleX: 1 }}
                 animate={{ scaleX: 0 }}
                 exit={{ scaleX: 0 }}
-                transition={{ duration: 0.5, ease: 'easeInOut' }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
             />
         </>
     );
