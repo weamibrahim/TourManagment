48952ea10352c7c5dccd9982d36f2093b1cbc50b
weam <weamibrahim46@gmail.com>
2024-08-28 20:48:15 +0300

    add section about in home

--- frontend/src/Components/Pages/Home/Home.jsx
+++ frontend/src/Components/Pages/Home/Home.jsx
@@ -1,137 +1,255 @@
-import './Home.css';
-import Search from '../../Parts/Search/Search';
-import { Helmet } from 'react-helmet-async';
+import "./Home.css";
+
+import { Helmet } from "react-helmet-async";
 import { AiFillSafetyCertificate } from "react-icons/ai";
 import { MdOutlinePriceChange } from "react-icons/md";
 import { CgViewComfortable } from "react-icons/cg";
-import SwiperSlides from '../../Parts/Swiper/Swiper';
-import PageTransition from '../../Parts/Animation/PageTransition';
-import SectionScroll from '../../Parts/Animation/ScrollSection';
-import {motion} from 'framer-motion';
+import SwiperSlides from "../../Parts/Swiper/Swiper";
+import PageTransition from "../../Parts/Animation/PageTransition";
+import SectionScroll from "../../Parts/Animation/ScrollSection";
+import { motion } from "framer-motion";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 function Home() {
-    return (
-        <>
-        <Helmet>
-            <title>Home</title>
-        </Helmet>
-      
-        <div className="container my-5 ">
-         
-            <div className="row m-3 bg-body-tertiary p-5 rounded-5 ">
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
 
-                <motion.div className="col-md-6  "
-                initial={{ opacity: 0 , x:-100}}
-                animate={{ opacity: 1 ,x:0}}
-                exit={{ opacity: 0 ,x:0}}
-                transition={{duration: 0.5 , ease: 'easeInOut' , delay: 0.5 }}
-                >
-                  <div  >
-                    <h2  style={{  fontFamily: "Sofia"}}>Explore the world <span className='text-warning'> with us! </span></h2>
-              <p className='my-5'> Discover breathtaking destinations,
-                 unique experiences and unforgettable adventures,
-                 Book your perfect tour with ease and embark on an unforgettable journey.
-              </p> </div> </motion.div>
-              
-              <motion.div 
-              className='col-md-6 '
-                initial={{ opacity: 0 , x:100}}
-                animate={{ opacity: 1 ,x:0}}
-                exit={{ opacity: 0 ,x:0}}
-                transition={{duration: 0.5 , ease: 'easeInOut' , delay: 0.5 }}
-                >
-                <div className="ms-auto">
-
-                    <div className="row">
-                         <div className="col-md-4 ">
-                            <img className='rounded-4 img-fluid' src="https://img.freepik.com/free-photo/vertical-shot-beautiful-eiffel-tower-captured-paris-france_181624-45445.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user" />
-                            </div>
-                        <div className="col-md-4 my-4">
-                            <img className='rounded-4 img-fluid' src="https://img.freepik.com/free-photo/ferris-wheel-park_74190-5032.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user" />
-                            </div>
-                            
-                           
-                            <div className="col-md-4 my-5">
-                            <img className='rounded-4 img-fluid ' src="https://img.freepik.com/free-photo/palm-alley-tropical-egyptian-beach_155003-10002.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user" />
-                            </div>
-
-                            
-                    </div>
-                </div>
-              </motion.div>
-            </div>
-            <Search/>
+      <div className="container my-5 ">
+        <div className="row m-3 bg-body-tertiary p-5 rounded-5 ">
           
+          <motion.div
+            className="col-md-6  "
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
+          >
             <div>
-                <h1 style={{  fontFamily: "Sofia"}}>Gallery</h1>
-<SwiperSlides/>
-                <div className='row m-3'>
-                  
-  <div className='col-md-4 my-1' >
-  <SectionScroll>
-    <img src='https://img.freepik.com/free-photo/sky-beautiful-vacation-ocean-house_1203-5310.jpg?t=st=1721687364~exp=1721690964~hmac=26bf9c28d583537338905228143835b0014458c03de6c7d45471ce891f29cb60&w=360' className='img-fluid  rounded-4'/>
-    </SectionScroll>
-  </div>
+              <h2 style={{ fontFamily: "Sofia" }}>
+                Explore the world
+                <span className="text-warning"> with us! </span>
+              </h2>
+              <p className="my-5">
+                
+                Discover breathtaking destinations, unique experiences and
+                unforgettable adventures, Book your perfect tour with ease and
+                embark on an unforgettable journey.
+              </p>
+            </div>
+          </motion.div>
 
-  <div className='col-md-8' >
-    <div className='row mb-2'>
-      <div className='col-md-6 my-1'>
-      <SectionScroll>
-       
-       <img className='img-fluid  rounded-4' src="https://img.freepik.com/free-photo/3d-render-yacht-tropical-island_1048-5501.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user"/>
-       </SectionScroll>
-      </div>
-      <div className='col-md-6 my-1'>
-      <SectionScroll>
-        <img className='img-fluid  rounded-4' src="https://img.freepik.com/free-photo/aerial-view-lao-lading-island-krabi-thailand_335224-1184.jpg?t=st=1721687587~exp=1721691187~hmac=575b6d708fba15f3eb36b1d3fc5c6c143bbe877ee066e9047c821a247a08faca&w=1060"/>
-        </SectionScroll>
-      </div>
-    </div>
-    <div className='row'>
-      <div className='col-12 my-1'>
-      <SectionScroll>
-        <img className='img-fluid rounded-4' src="https://img.freepik.com/free-photo/wide-shot-kualoa-ranch-hawaii-usa_181624-11701.jpg?t=st=1721687950~exp=1721691550~hmac=cd9c36bf9e285199fccfbec159b98ec162c349ff79aa6fe1e96230257338665d&w=1060" style={{maxHeight: "270px", width: "100%"}} />
-        </SectionScroll>
-      </div>
-    </div>
+          <motion.div
+            className="col-md-6 "
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
+          >
+            <div className="ms-auto">
+              <div className="row">
+                <div className="col-md-4 ">
+                  <img
+                    className="rounded-4 img-fluid"
+                    src="https://img.freepik.com/free-photo/vertical-shot-beautiful-eiffel-tower-captured-paris-france_181624-45445.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user"
+                  />
+                </div>
+                <div className="col-md-4 my-4">
+                  <img
+                    className="rounded-4 img-fluid"
+                    src="https://img.freepik.com/free-photo/ferris-wheel-park_74190-5032.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user"
+                  />
+                </div>
+
+                <div className="col-md-4 my-5">
+                  <img
+                    className="rounded-4 img-fluid "
+                    src="https://img.freepik.com/free-photo/palm-alley-tropical-egyptian-beach_155003-10002.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user"
+                  />
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+
+        {/* ////////////////////////////////////////////////////About///////////////////////////////////////////////////////// */}
+<div className="row my-5">
+  
+<div className="col-md-6 col-sm-12">
+  <div className="position-relative">
+    <img
+      src="https://img.freepik.com/free-photo/kelingking-beach-nusa-penida-island-bali-indonesia_335224-335.jpg?ga=GA1.1.1848435922.1723918661&semt=ais_hybrid"
+      className="main_img img-fluid"
+      alt="Main"
+    />
+    <img
+      src="https://img.freepik.com/free-photo/view-hot-air-balloons-moroccan-sky_268835-3945.jpg?ga=GA1.1.1848435922.1723918661&semt=ais_hybrid"
+      className="branch_img img-fluid"
+      alt="Branch"
+    />
+    <img
+      src="https://img.freepik.com/free-photo/infinity-caribbean-resort-vacation-tree_1203-5297.jpg?ga=GA1.1.1848435922.1723918661&semt=ais_hybrid"
+      className="branch2_img img-fluid"
+      alt="Branch2"
+    />
+  </div>
+</div>
+
+  <div className="col-md-6">
+    <h4 style={{ fontFamily: "Sofia" }} className="text-warning">About Us</h4>
+  
+      <span className="fw-bold fs-3">We  Recommend Beautiful 
+         </span>
+        
+         <p className="fs-3"> Beautiful  Distanstions Every Month</p>
+         <p>Let's chose your dream destination here we provide  many destination  and we offer best destination every week</p>
+         <div className="row">
+          <div className="col-md-3 text-center bg-body-secondary p-3 rounded-3 m-2" >
+            
+          <VisibilitySensor partialVisibility>
+        {({ isVisible }) => (
+          <div className="fw-bold">
+            {isVisible ? <CountUp start={0} end={1000} duration={2.75} /> : "0"}+
+          </div>
+        )}
+      </VisibilitySensor>
+            <div>Our Explorers</div>
+
+          </div>
+          <div className="col-md-3 text-center bg-body-secondary p-3 rounded-3 m-2  " >
+          <VisibilitySensor partialVisibility>
+        {({ isVisible }) => (
+          <div className="fw-bold">
+            {isVisible ? <CountUp start={0} end={100} duration={2.75} /> : "0"}+
+          </div>
+        )}
+      </VisibilitySensor>
+          <div>Destination</div>
+          </div>
+          <div className="col-md-3 text-center bg-body-secondary p-3 rounded-3 m-2" >
+          <VisibilitySensor partialVisibility>
+        {({ isVisible }) => (
+          <div className="fw-bold">
+            {isVisible ? <CountUp start={0} end={20} duration={2.75} /> : "0"}+
+          </div>
+        )}
+      </VisibilitySensor>
+          <div>Years Experience</div>
+          </div>
+          
+         </div>
+  </div>
 </div>
 
 
-                
-                </div>
-            </div>
-          
-                
-                <div className='row my-5 mx-3 bg-body-tertiary p-5 rounded-4'>
-                    <div className='col-md-4  ' >
-                    <SectionScroll>
-                        <h1>Experince  the new adventure</h1>
-                        <p>Explore the world with us! Discover breathtaking destinations, unique experiences and unforgettable adventures, Book your perfect tour with ease and embark on an unforgettable journe</p>
-                        <div ><AiFillSafetyCertificate className='fs-3 text-warning'/>
-                        <span className='fs-4 text-dark mx-2'>Safe Travelling</span>
-                        <p>Explore the world with us! Discover breathtaking destinations, </p>
-                        </div>
-                        <div ><MdOutlinePriceChange className='fs-3 text-warning' />
-                        <span className='fs-4 text-dark mx-2'>Affordable price</span>
-                        <p>Explore the world with us! Discover breathtaking des</p>
-                        </div>
-                        <div><CgViewComfortable className='fs-3 text-warning'/>
-                        <span className='fs-4 text-dark mx-2'>comfort accomndation</span>
-                        <p>Explore the world with us! Discover breathtaking destinations, unique experiences and unforgettable adventures,e</p>
-                       
-                        </div>
-                        </SectionScroll>
-                      </div>
 
-                      <div className='col-md-8'>
-                      <SectionScroll>
-                        <img src="https://img.freepik.com/free-photo/woman-walking-railay-beach-krabi-thailand_335224-1206.jpg?t=st=1721686134~exp=1721689734~hmac=873488df5e108cdbc09094dbda0658033e6f67e9f3608015c256dcc735275e56&w=1060" className='img-fluid rounded-4'/>    
-                        </SectionScroll>
-                      </div>
-           </div>
+
+
+
+
+
+
+
+
+
+
+      {/* //////////////////////////////////////////////////////Gallery///////////////////////////////////////////////////////// */}
+
+        <div>
+          <h1 style={{ fontFamily: "Sofia" }}>Gallery</h1>
+          <SwiperSlides />
+          <div className="row m-3">
+            <div className="col-md-4 my-1">
+              <SectionScroll>
+                <img
+                  src="https://img.freepik.com/free-photo/sky-beautiful-vacation-ocean-house_1203-5310.jpg?t=st=1721687364~exp=1721690964~hmac=26bf9c28d583537338905228143835b0014458c03de6c7d45471ce891f29cb60&w=360"
+                  className="img-fluid  rounded-4"
+                />
+              </SectionScroll>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row mb-2">
+                <div className="col-md-6 my-1">
+                  <SectionScroll>
+                    <img
+                      className="img-fluid  rounded-4"
+                      src="https://img.freepik.com/free-photo/3d-render-yacht-tropical-island_1048-5501.jpg?ga=GA1.1.1070693569.1706463458&semt=ais_user"
+                    />
+                  </SectionScroll>
+                </div>
+                <div className="col-md-6 my-1">
+                  <SectionScroll>
+                    <img
+                      className="img-fluid  rounded-4"
+                      src="https://img.freepik.com/free-photo/aerial-view-lao-lading-island-krabi-thailand_335224-1184.jpg?t=st=1721687587~exp=1721691187~hmac=575b6d708fba15f3eb36b1d3fc5c6c143bbe877ee066e9047c821a247a08faca&w=1060"
+                    />
+                  </SectionScroll>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 my-1">
+                  <SectionScroll>
+                    <img
+                      className="img-fluid rounded-4"
+                      src="https://img.freepik.com/free-photo/wide-shot-kualoa-ranch-hawaii-usa_181624-11701.jpg?t=st=1721687950~exp=1721691550~hmac=cd9c36bf9e285199fccfbec159b98ec162c349ff79aa6fe1e96230257338665d&w=1060"
+                      style={{ maxHeight: "270px", width: "100%" }}
+                    />
+                  </SectionScroll>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        
-        </>
-    );
+
+        <div className="row my-5 mx-3 bg-body-tertiary p-5 rounded-4">
+          <div className="col-md-4  ">
+            <SectionScroll>
+              <h1>Experince the new adventure</h1>
+              <p>
+                Explore the world with us! Discover breathtaking destinations,
+                unique experiences and unforgettable adventures, Book your
+                perfect tour with ease and embark on an unforgettable journe
+              </p>
+              <div>
+                <AiFillSafetyCertificate className="fs-3 text-warning" />
+                <span className="fs-4 text-dark mx-2">Safe Travelling</span>
+                <p>
+                  Explore the world with us! Discover breathtaking destinations,{" "}
+                </p>
+              </div>
+              <div>
+                <MdOutlinePriceChange className="fs-3 text-warning" />
+                <span className="fs-4 text-dark mx-2">Affordable price</span>
+                <p>Explore the world with us! Discover breathtaking des</p>
+              </div>
+              <div>
+                <CgViewComfortable className="fs-3 text-warning" />
+                <span className="fs-4 text-dark mx-2">
+                  comfort accomndation
+                </span>
+                <p>
+                  Explore the world with us! Discover breathtaking destinations,
+                  unique experiences and unforgettable adventures,e
+                </p>
+              </div>
+            </SectionScroll>
+          </div>
+
+          <div className="col-md-8">
+            <SectionScroll>
+              <img
+                src="https://img.freepik.com/free-photo/woman-walking-railay-beach-krabi-thailand_335224-1206.jpg?t=st=1721686134~exp=1721689734~hmac=873488df5e108cdbc09094dbda0658033e6f67e9f3608015c256dcc735275e56&w=1060"
+                className="img-fluid rounded-4"
+              />
+            </SectionScroll>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PageTransition(Home);
--- frontend/src/Components/Pages/Login/Login.jsx
+++ frontend/src/Components/Pages/Login/Login.jsx
@@ -1,41 +1,46 @@
-import './Login.css';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useLogin } from '../../../Contexts/LoginContext';
-import PageTransition from '../../Parts/Animation/PageTransition';
+import "./Login.css";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useLogin } from "../../../Contexts/LoginContext";
+import PageTransition from "../../Parts/Animation/PageTransition";
 const Login = () => {
   const navigate = useNavigate();
-  const {  setIsLogin } = useLogin();
+  const { setIsLogin } = useLogin();
 
   const validationSchema = Yup.object({
-    email: Yup.string().email('Invalid email address').required('Required'),
-    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Required'),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Required"),
   });
 
   const handleSubmit = async (values, { setSubmitting, setErrors }) => {
     try {
-      const response = await fetch('https://tour-managment-three.vercel.app/api/users/login', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      });
+      const response = await fetch(
+        "https://tour-managment-three.vercel.app/api/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
+      );
 
       const data = await response.json();
       if (data.status === "success") {
-        navigate('/');
+        navigate("/");
         localStorage.setItem("accessToken", JSON.stringify(data.accessToken));
         localStorage.setItem("user", JSON.stringify(data.user));
         setIsLogin(true);
-        if(data.user.role === "admin") navigate('/dashboard')
+        if (data.user.role === "admin") navigate("/dashboard");
       } else {
         setErrors({ serverError: data.message });
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     } finally {
       setSubmitting(false);
     }
@@ -46,41 +51,77 @@ const Login = () => {
       <Helmet>
         <title>Login</title>
       </Helmet>
-      <div >
-       
-        <div className='d-flex justify-content-center my-5 '>
-        <div className='row w-75 h-100 shadow p-3 mb-5 bg-body-tertiary rounded'>
-         
-          <div className='col-md-6 login d-flex flex-column justify-content-center align-items-center rounded-start ' >
-          <h1 className='text-center my-2' style={{  fontFamily: "Sofia"}}>Login</h1>
-          <Formik
-            initialValues={{ email: '', password: '' }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting, errors }) => (
-              <Form   className=''>
-                <div className="mb-3 px-5 ">
-                  <label htmlFor="email" className="form-label">EMAIL</label>
-                  <Field type="email" name="email" className="form-control " />
-                  <ErrorMessage name="email" component="div"  className='alert alert-danger my-2'/>
-                </div>
-                <div className="mb-3 px-5">
-                  <label htmlFor="password" className="form-label">PASSWORD</label>
-                  <Field type="password" name="password" className="form-control" />
-                  <ErrorMessage name="password" component="div"className='alert alert-danger my-2' />
-                </div>
-                {errors.serverError && <div className='alert alert-danger my-2'>{errors.serverError}</div>}
-                <div className='d-flex justify-content-center px-5'>
-                  <button type="submit" className="btn btn-primary w-100" disabled={isSubmitting}>Login</button>
-                </div>
-                <p className='text-center mt-3'>Don't have an account? <NavLink to="/register">Register</NavLink></p>
-              </Form>
-            )}
-          </Formik>
-          </div>
-          <div className='col-md-6 p-0 rounded-end ' >
-            <img  src='https://img.freepik.com/free-vector/access-control-system-abstract-concept_335657-3180.jpg?ga=GA1.2.1070693569.1706463458&semt=sph' className='img-fluid rounded-start '/>
+      <div>
+        <div className="d-flex justify-content-center my-5 ">
+          <div className="row w-75 h-100 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div className="col-md-6 login d-flex flex-column justify-content-center align-items-center rounded-start ">
+              <h1 className="text-center my-2" style={{ fontFamily: "Sofia" }}>
+                Login
+              </h1>
+              <Formik
+                initialValues={{ email: "", password: "" }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+              >
+                {({ isSubmitting, errors }) => (
+                  <Form className="">
+                    <div className="mb-3 px-5 ">
+                      <label htmlFor="email" className="form-label">
+                        EMAIL
+                      </label>
+                      <Field
+                        type="email"
+                        name="email"
+                        className="form-control "
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="alert alert-danger my-2"
+                      />
+                    </div>
+                    <div className="mb-3 px-5">
+                      <label htmlFor="password" className="form-label">
+                        PASSWORD
+                      </label>
+                      <Field
+                        type="password"
+                        name="password"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="alert alert-danger my-2"
+                      />
+                    </div>
+                    {errors.serverError && (
+                      <div className="alert alert-danger my-2">
+                        {errors.serverError}
+                      </div>
+                    )}
+                    <div className="d-flex justify-content-center px-5">
+                      <button
+                        type="submit"
+                        className="btn btn-primary w-100"
+                        disabled={isSubmitting}
+                      >
+                        Login
+                      </button>
+                    </div>
+                    <p className="text-center mt-3">
+                      Don't have an account?{" "}
+                      <NavLink to="/register">Register</NavLink>
+                    </p>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+            <div className="col-md-6 p-0 rounded-end ">
+              <img
+                src="https://img.freepik.com/free-vector/access-control-system-abstract-concept_335657-3180.jpg?ga=GA1.2.1070693569.1706463458&semt=sph"
+                className="img-fluid rounded-start "
+              />
             </div>
           </div>
         </div>
